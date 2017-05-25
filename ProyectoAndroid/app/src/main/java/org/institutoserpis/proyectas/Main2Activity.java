f2f7d201df1ed52fa470c7a2e0fafc62dccb7f3f
Costaico <alejandrocm98@live.com>
2017-05-25 10:00:58 +0200

    Proyecto Android Studio, abrir EXPLICACION

---
+++ ProyectoAndroid/app/src/main/java/org/institutoserpis/proyectas/Main2Activity.java
@@ -0,0 +1,25 @@
+package org.institutoserpis.proyectas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button btn = (Button) findViewById(R.id.gotoreloj);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+}
